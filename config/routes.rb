6362a050be09c162332f3f584d07d611648d9d74
Bilal <ranabilalrajput09@gmail.com>
2023-09-01 09:40:28 +0200

    Add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      post "login", to: "users#login"
+      post "signup", to: "users#signup"
+      get "signout", to: "users#signout"
+      get "current_user", to: "users#current_user"
+    end
+  end
 end
---
+++ db/migrate/20230831164046_create_adventures.rb
@@ -0,0 +1,10 @@
+class CreateAdventures < ActiveRecord::Migration[7.0]
+  def change
+    create_table :adventures do |t|
+      t.string :name 
+      t.string :picture
+      t.text :description
+      t.timestamps
+    end
+  end
+end
