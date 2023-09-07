da87b9865c58f9d693e1ed09ded0e123e9f725d8
kalbek <access2kaleb@gmail.com>
2023-09-07 15:27:20 +0300

    Attempt to fix rubocop.yml IgnoredMethods errors.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   namespace :api do
     namespace :v1 do
       post 'login', to: 'users#login'
@@ -11,11 +9,9 @@ Rails.application.routes.draw do
       get 'signout', to: 'users#signout'
       get 'current_user', to: 'users#current_user'
       get 'adventures', to: 'adventures#index'
-      # get 'adventures/:id', to: 'adventures#show', on: :member
       resources :reservations, only: [:create]
       post 'fetch_reservations', to: 'reservations#fetch_reservations'
       delete 'delete_reservation', to: 'reservations#destroy'
-
 
       resources :adventures do
         member do
