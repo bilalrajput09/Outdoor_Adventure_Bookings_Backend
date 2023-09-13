18400669ee1b62cfcd2652cf47cfbeec0545fb3c
Bilal <ranabilalrajput09@gmail.com>
2023-09-13 14:21:11 +0200

    Fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,9 +5,9 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://outdoor-adventures-booking.onrender.com/'
+    origins '*'
 
     resource '/api/v1/*',
              headers: :any,
