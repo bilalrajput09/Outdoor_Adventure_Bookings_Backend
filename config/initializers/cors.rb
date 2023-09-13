8aa5ab27582fd2f8942d7a2b3e8f7f6c3982bbde
Bilal <ranabilalrajput09@gmail.com>
2023-09-13 13:01:36 +0200

    Fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,4 +13,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
              headers: :any,
              methods: %i[get post put patch delete options head]
   end
+
+  allow do
+    origins 'https://outdoor-adventures-booking.onrender.com/'
+
+    resource '/api/v1/*',
+             headers: :any,
+             methods: %i[get post put patch delete options head]
+  end
 end
